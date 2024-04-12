4c3e1608cc59e9bddf4c24d6bf474e1bf88d59a2
Mr. Kalulu <165420915+WakakaTz@users.noreply.github.com>
2024-04-12 18:17:29 +0300

    Update config.js

--- config.js
+++ config.js
@@ -17,10 +17,10 @@ global.blockJids= process.env.BLOCK_JID || "null"
 global.DATABASE_URL = process.env.DATABASE_URL || ""
 
 global.timezone= process.env.TZ || process.env.TIME_ZONE || "Asia/Karachi";
-global.github=process.env.GITHUB|| "https://github.com/SuhailTechInfo/Suhail-Md";
-global.gurl  =process.env.GURL  || "https://whatsapp.com/channel/0029Va9thusJP20yWxQ6N643";
-global.website=process.env.GURL || "https://whatsapp.com/channel/0029Va9thusJP20yWxQ6N643" ; 
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/d5b1c3544fedc23e11a06.jpg" ; // SET LOGO FOR IMAGE 
+global.github=process.env.GITHUB|| "https://github.com/WakakaTz/Suhail-Md";
+global.gurl  =process.env.GURL  || "";
+global.website=process.env.GURL || "" ; 
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/08ac5b27abfb6f5ac4397.png" ; // SET LOGO FOR IMAGE 
 
 
 
@@ -42,7 +42,7 @@ global.disablepm = process.env.DISABLE_PM || "false"
 global.disablegroup = process.env.DISABLE_GROUPS || "false", // disable bot in groups when public mode
 
 global.MsgsInLog = process.env.MSGS_IN_LOG|| "false" // "true"  to see messages , "log" to open logs , "false" to hide logs messages
-global.userImages= process.env.USER_IMAGES || "https://telegra.ph/file/758549f0de2219884f387.jpg,https://telegra.ph/file/9498496b010dfb5900b80.jpg,https://telegra.ph/file/59bf80efbb6e6576b5a52.jpg,https://telegra.ph/file/ee0e191363161e88e4743.jpg" // ramadan Theme Images
+global.userImages= process.env.USER_IMAGES || "https://telegra.ph/file/08ac5b27abfb6f5ac4397.png" // ramadan Theme Images
 global.waPresence= process.env.WAPRESENCE ||  "set according to your need" ; // 'unavailable' | 'available' | 'composing' | 'recording' | 'paused'
 
 
@@ -62,7 +62,7 @@ global.api_smd = "https://api-smd-1.vercel.app"
 global.scan = "https://suhail-md-vtsf.onrender.com/";
 
 
-global.SESSION_ID = process.env.SESSION_ID ||  "SUHAIL_20_04_04_01_ewogICJjcmVkcy5qc29uIjogIntcIm5vaXNlS2V5XCI6e1wicHJpdmF0ZVwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiQUNQNk5ZTXFhTDI4OVovR1h1S3drbThxTWJDRHhEU2RKaHVjcGcwU2RWWT1cIn0sXCJwdWJsaWNcIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcInpWSENCaVJ5alJmVjdEVGo4ZjZQZFEyVzlTVERFUFk3S3pHc3hHWHhSREk9XCJ9fSxcInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyXCI6e1wicHJpdmF0ZVwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwicUZ6WHpPR0RmRldrelk2bmFRN0lrV3A2bHdyUis3ZWQxeUxOcTFWQnNWdz1cIn0sXCJwdWJsaWNcIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcIjAzTEJVUnhxQ2Z0QlVzTWJHY3ZUaW80eHFCclpONThleUJ5cGdWaEVlWFE9XCJ9fSxcInNpZ25lZElkZW50aXR5S2V5XCI6e1wicHJpdmF0ZVwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiZUNHVEtjQTZGc1VscEZLRlRHVmUzNkZlQnlqaW1ZOVJXRHVISGZiaG5HND1cIn0sXCJwdWJsaWNcIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcIkVpU2xEQ3hudDl3V01UMldrT3hGN0I1K1JibTNkdDJkU0tQRFVMRmZGejA9XCJ9fSxcInNpZ25lZFByZUtleVwiOntcImtleVBhaXJcIjp7XCJwcml2YXRlXCI6e1widHlwZVwiOlwiQnVmZmVyXCIsXCJkYXRhXCI6XCJPRmtySmlQc3dsK24wTVpWalRocHF4MVlyVDVHZ3BySTFYSXBhcE5tWlZBPVwifSxcInB1YmxpY1wiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiRUpUUVR4RjRMa3pPT0V3bnN2SmV1ZDRYWUhOQnM3RVJ3QmxzaDZDWmVnND1cIn19LFwic2lnbmF0dXJlXCI6e1widHlwZVwiOlwiQnVmZmVyXCIsXCJkYXRhXCI6XCI3ak5hYm1LR1R1QlkvRjJ1V0NuTkFKc3NSVHViY09HNTVrc0tidEdKM2lGeUhONGd6UVJkNXh1YThtbUVTdG5aL3hUMERhNWZjNVk2SnlWOUhGMGVpdz09XCJ9LFwia2V5SWRcIjoxfSxcInJlZ2lzdHJhdGlvbklkXCI6NzEsXCJhZHZTZWNyZXRLZXlcIjpcIjRmOURSdkxTT2U1SUpIMmFtSEYvTHRtTWZlTnhtOTk5Q3JpbXN4MWNiRG89XCIsXCJwcm9jZXNzZWRIaXN0b3J5TWVzc2FnZXNcIjpbe1wia2V5XCI6e1wicmVtb3RlSmlkXCI6XCIyNTU2MjY3NTM4MThAcy53aGF0c2FwcC5uZXRcIixcImZyb21NZVwiOnRydWUsXCJpZFwiOlwiN0E2QTBBNjg0NTYzNkJGODFGNEQ1NjBEQ0U5MDY5RDJcIn0sXCJtZXNzYWdlVGltZXN0YW1wXCI6MTcxMjAwMTg0Mn1dLFwibmV4dFByZUtleUlkXCI6MzEsXCJmaXJzdFVudXBsb2FkZWRQcmVLZXlJZFwiOjMxLFwiYWNjb3VudFN5bmNDb3VudGVyXCI6MCxcImFjY291bnRTZXR0aW5nc1wiOntcInVuYXJjaGl2ZUNoYXRzXCI6ZmFsc2V9LFwiZGV2aWNlSWRcIjpcIncyUi1wZzRJUjdLUXZlTEVzNWlKR0FcIixcInBob25lSWRcIjpcIjdlNjM3YjUyLTBkZmMtNGI3Zi1iMTI4LTExYTk3NWM0NTVlZlwiLFwiaWRlbnRpdHlJZFwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwibGZSNG9UbEVjTUpLKyt3OThRcWVXL2dCYzU0PVwifSxcInJlZ2lzdGVyZWRcIjp0cnVlLFwiYmFja3VwVG9rZW5cIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcImZrQkt3QUpRWWc0aU9GYTVMTEkwdzljWE5GZz1cIn0sXCJyZWdpc3RyYXRpb25cIjp7fSxcInBhaXJpbmdDb2RlXCI6XCJTWDZKS1pHNFwiLFwibWVcIjp7XCJpZFwiOlwiMjU1NjI2NzUzODE4OjQzQHMud2hhdHNhcHAubmV0XCIsXCJuYW1lXCI6XCJ+Qy4gRS4gTyBXYWtha2FcIixcImxpZFwiOlwiMjMwMTUwMjM1MDA1MDM0OjQzQGxpZFwifSxcImFjY291bnRcIjp7XCJkZXRhaWxzXCI6XCJDTXVUa0cwUXFxYXNzQVlZQVNBQUtBQT1cIixcImFjY291bnRTaWduYXR1cmVLZXlcIjpcImNZSEVHdnJpa2pLUFJTa0N2NEx3YXJ2ZldkRkhxNEtyQ3I5RUIxVkRsQm89XCIsXCJhY2NvdW50U2lnbmF0dXJlXCI6XCJJVDdreGRkYUI5VUNwZU5NOUV1UTIwRk12M1hlcWNWTUxKMkVBOG1zUnM2WEsxcE91eUZBVTIwQ2NudHAvQ2RJOHhPV0JoaUxBOUJ5elZPNENLTzFEUT09XCIsXCJkZXZpY2VTaWduYXR1cmVcIjpcIlZxbWp3c2pPeXVjQS9qMFNCWjZJTE9MZ1Yvb1R1alJTNWpwalRMc0FrZVlJUVd4N0ptNXhISHpKRHBJRnpSWE9CQlNQTWVRKzl1NjRrTjFCVHJjQmpnPT1cIn0sXCJzaWduYWxJZGVudGl0aWVzXCI6W3tcImlkZW50aWZpZXJcIjp7XCJuYW1lXCI6XCIyNTU2MjY3NTM4MTg6NDNAcy53aGF0c2FwcC5uZXRcIixcImRldmljZUlkXCI6MH0sXCJpZGVudGlmaWVyS2V5XCI6e1widHlwZVwiOlwiQnVmZmVyXCIsXCJkYXRhXCI6XCJCWEdCeEJyNjRwSXlqMFVwQXIrQzhHcTczMW5SUjZ1Q3F3cS9SQWRWUTVRYVwifX1dLFwicGxhdGZvcm1cIjpcInNtYmFcIixcImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcFwiOjE3MTIwMDE4MzgsXCJteUFwcFN0YXRlS2V5SWRcIjpcIkFBQUFBTlBsXCJ9IiwKICAiYXBwLXN0YXRlLXN5bmMta2V5LUFBQUFBTlBsLmpzb24iOiAie1wia2V5RGF0YVwiOlwiQWp5OUlwK3IwT3VmdlRDZUhLRVhHSjdseFErWGpLMzQzQUpoQmhEeHJ0QT1cIixcImZpbmdlcnByaW50XCI6e1wicmF3SWRcIjoyMjg4NTQyMTgsXCJjdXJyZW50SW5kZXhcIjoyLFwiZGV2aWNlSW5kZXhlc1wiOlswLDJdfSxcInRpbWVzdGFtcFwiOlwiMFwifSIKfQ==" ;
+global.SESSION_ID = process.env.SESSION_ID ||  "" ;
 
 
 module.exports = {
@@ -72,19 +72,19 @@ module.exports = {
   HANDLERS: process.env.PREFIX  || ".",
   BRANCH  : process.env.BRANCH  || "main",
   VERSION : process.env.VERSION || "1.3.1",
-  caption : process.env.CAPTION || "©sᴜʜᴀɪʟ²²¹-ᴍᴅ" , // ```『 ᴘᴏᴡᴇʀᴇᴅ ʙʏ sᴜʜᴀɪʟ²²¹-ᴍᴅ 』```", //*『sᴜʙsᴄʀɪʙᴇ • sᴜʜᴀɪʟ ᴛᴇᴄʜ』*\n youtube.com/@suhailtechinfo0"),
+  caption : process.env.CAPTION || "Wakaka-Bot" , // ```『 ᴘᴏᴡᴇʀᴇᴅ ʙʏ Wakaka Tanzania 』```", //*『FOLLOW US • WAKAKA TANZANIA』*\n https://www.instagram.com/wakaka_tanzania/"),
  
   author : process.env.PACK_AUTHER|| "Wakaka-Bot",
   packname: process.env.PACK_NAME || "wakaka♥️",
-  botname : process.env.BOT_NAME  || "WakakaBot",
-  ownername:process.env.OWNER_NAME|| "Kalulu",
+  botname : process.env.BOT_NAME  || "Wakaka-Bot",
+  ownername:process.env.OWNER_NAME|| "Mr. Kalulu",
 
 
   errorChat : process.env.ERROR_CHAT || "",
   KOYEB_API : process.env.KOYEB_API  || "false",
 
   REMOVE_BG_KEY : process.env.REMOVE_BG_KEY  || "",
-  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "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",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
   HEROKU_APP_NAME:process.env.HEROKU_APP_NAME|| "",
   antilink_values:process.env.ANTILINK_VALUES|| "all",
