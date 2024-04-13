146871f6bd129d3837e8ac994a964ce8195ae294
Mr. Kalulu <165420915+WakakaTz@users.noreply.github.com>
2024-04-13 18:33:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -16,10 +16,10 @@ global.allowJids= process.env.ALLOW_JID || "null"
 global.blockJids= process.env.BLOCK_JID || "null"
 global.DATABASE_URL = process.env.DATABASE_URL || ""
 
-global.timezone= process.env.TZ || process.env.TIME_ZONE || "Asia/Karachi";
-global.github=process.env.GITHUB|| "https://github.com/WakakaTz/Suhail-Md";
-global.gurl  =process.env.GURL  || "https://whatsapp.com/channel/0029VabUjmHG3R3pY0mEro0I";
-global.website=process.env.GURL || "https://whatsapp.com/channel/0029VabUjmHG3R3pY0mEro0I" ; 
+global.timezone= process.env.TZ || process.env.TIME_ZONE || "Tanzania";
+global.github=process.env.GITHUB|| "https://github.com/WakakaTz/Wakaka-Bot";
+global.gurl  =process.env.GURL  || "";
+global.website=process.env.GURL || "" ; 
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/08ac5b27abfb6f5ac4397.png" ; // SET LOGO FOR IMAGE 
 
 
@@ -62,7 +62,7 @@ global.api_smd = "https://api-smd-1.vercel.app"
 global.scan = "https://suhail-md-vtsf.onrender.com/";
 
 
-global.SESSION_ID = process.env.SESSION_ID ||  "" ;
+global.SESSION_ID = process.env.SESSION_ID ||  "SUHAIL_14_17_04_12_ewogICJjcmVkcy5qc29uIjogIntcIm5vaXNlS2V5XCI6e1wicHJpdmF0ZVwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwicUVJcWkxbi9HTkorK2V3dXRNOXlwY2FwSjVzTjdFTVo4RTc2Ynhyd3Rscz1cIn0sXCJwdWJsaWNcIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcIi9PTEdwczJDcWpUNXU3WWNPRlZ0UXlEY0dUQ2ZDdG02UFpFeVpJbndqd1U9XCJ9fSxcInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyXCI6e1wicHJpdmF0ZVwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiMEppVjMvMk83ajh6M3AwSG5GK0UxbjFpVzJzVDl0QnlONzRhR1N4VWUxVT1cIn0sXCJwdWJsaWNcIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcImxBOFB0dXVPbFZPRXRLTXAwSGd5c3RVV1RTbWhwOTlUMDZsZzlZRldVVFU9XCJ9fSxcInNpZ25lZElkZW50aXR5S2V5XCI6e1wicHJpdmF0ZVwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiQ01oWThiWnkzUUMwQ2RnUTNXYnlXR2JNSWFuZnJUYW5vNTg1aTZTamZtaz1cIn0sXCJwdWJsaWNcIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcImhJTjdxc3dheE9FeXVJakVLc1ZXVXhYZFlFaVNDRWFCWGtaeXNGeU9RRnM9XCJ9fSxcInNpZ25lZFByZUtleVwiOntcImtleVBhaXJcIjp7XCJwcml2YXRlXCI6e1widHlwZVwiOlwiQnVmZmVyXCIsXCJkYXRhXCI6XCJZUHNzU21vYmdnREFodjEwSjEwS3BGRzhPYlE1SUp1OFEzaWdQcStpUmtJPVwifSxcInB1YmxpY1wiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiVzlaU2o5cGhCeUVKSWV2c2ZJdjdKTUpJR1FRTUs0VFV2dlpoay8xQTJWcz1cIn19LFwic2lnbmF0dXJlXCI6e1widHlwZVwiOlwiQnVmZmVyXCIsXCJkYXRhXCI6XCJxMzB3c01JakN5VHJ6WWZ2SmNuU285dXR3ZHpxOGROY1J5SzFJb2gyTzdqR1FEaEFXY2tTOUgrbzNCVU85SDdxTjAxbEFCR0NvcHUxeWtZS0IxQlRBdz09XCJ9LFwia2V5SWRcIjoxfSxcInJlZ2lzdHJhdGlvbklkXCI6NDAsXCJhZHZTZWNyZXRLZXlcIjpcIk5tU1dmVnBRaE5QOHoxTTFLeEJXU1J1RHFaaERmK0JQTWFsa1ZoaGtpUzQ9XCIsXCJwcm9jZXNzZWRIaXN0b3J5TWVzc2FnZXNcIjpbe1wia2V5XCI6e1wicmVtb3RlSmlkXCI6XCIyNTU2MjY3NTM4MThAcy53aGF0c2FwcC5uZXRcIixcImZyb21NZVwiOnRydWUsXCJpZFwiOlwiREFBMjUyQjFEOTQ4MDdEOTJBNzc5RDA0MkYzOEMxMzJcIn0sXCJtZXNzYWdlVGltZXN0YW1wXCI6MTcxMjkzMTQ0MX0se1wia2V5XCI6e1wicmVtb3RlSmlkXCI6XCIyNTU2MjY3NTM4MThAcy53aGF0c2FwcC5uZXRcIixcImZyb21NZVwiOnRydWUsXCJpZFwiOlwiMkRFRkFDMDZDMzUzREM3MTM2QTk2MkQ2MTkxOTgyRkZcIn0sXCJtZXNzYWdlVGltZXN0YW1wXCI6MTcxMjkzMTQ0MX1dLFwibmV4dFByZUtleUlkXCI6MzEsXCJmaXJzdFVudXBsb2FkZWRQcmVLZXlJZFwiOjMxLFwiYWNjb3VudFN5bmNDb3VudGVyXCI6MSxcImFjY291bnRTZXR0aW5nc1wiOntcInVuYXJjaGl2ZUNoYXRzXCI6ZmFsc2V9LFwiZGV2aWNlSWRcIjpcIkZVbndvN2lvUlItcEt0Nk9MeUgzemdcIixcInBob25lSWRcIjpcImViMmE4ZDA0LWQyYzQtNDRlNy05MjMxLWE3ZTRmNDM0NzE2YlwiLFwiaWRlbnRpdHlJZFwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiZXpaNDBvWmV5MkNXcnhTUmF3cVE4bVd6TmRzPVwifSxcInJlZ2lzdGVyZWRcIjp0cnVlLFwiYmFja3VwVG9rZW5cIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcIll4Y05od1ZLVzRRQ3pSd09XdU4vQm4rRFVIQT1cIn0sXCJyZWdpc3RyYXRpb25cIjp7fSxcInBhaXJpbmdDb2RlXCI6XCJQQ0xUNFAxWVwiLFwibWVcIjp7XCJpZFwiOlwiMjU1NjI2NzUzODE4OjQ1QHMud2hhdHNhcHAubmV0XCIsXCJuYW1lXCI6XCJ" ;
 
 
 module.exports = {
@@ -91,9 +91,9 @@ module.exports = {
   HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
 
   aitts_Voice_Id : process.env.AITTS_ID || "37",
-  ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY  || "",
+  ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY  || "21cd82b99d1f7c88b4482f4eaec8a4c6",
   WORKTYPE: process.env.WORKTYPE || process.env.MODE|| "public",
-  LANG: ( process.env.THEME ||  "SUHAIL"  ).toUpperCase(),
+  LANG: ( process.env.THEME ||  "WAKAKA"  ).toUpperCase(),
 
 
 
